2a4a4857a776084f81cce2f39b2a83d249464a62
morsko1 <morssko1@gmail.com>
2021-06-21 13:40:43 +0300

    modified initDB function

--- product-service/initDB.js
+++ product-service/initDB.js
@@ -1,4 +1,5 @@
 import { Pool } from 'pg';
+import mockData from './mockData.json';
 
 const { PG_HOST, PG_PORT, PG_DBNAME, PG_USERNAME, PG_PASSWORD } = process.env;
 
@@ -34,6 +35,10 @@ const initDB = async () => {
     count - integer (There are no more products than this count in stock)
     */
 
+    const dropQuery = 'DROP TABLE IF EXISTS public.products CASCADE; DROP TABLE IF EXISTS public.stocks CASCADE;'
+    const dropQueryResult = await client.query(dropQuery);
+    console.log('dropQueryResult =\n', dropQueryResult);
+
     const createProductsTableQuery = {
       text: `
         CREATE EXTENSION IF NOT EXISTS "pgcrypto";
@@ -47,18 +52,40 @@ const initDB = async () => {
     const productsTableResult = await client.query(createProductsTableQuery);
     console.log('productsTableResult =\n', productsTableResult);
 
-    // const createStocksTableQuery = {
-    //   text: `
-    //     CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-    //     CREATE TABLE IF NOT EXISTS products(
-    //       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    //       title TEXT NOT NULL,
-    //       description TEXT,
-    //       price INT
-    //     )`
-    // };
-    // const stocksTableResult = await client.query(createStocksTableQuery);
-    // console.log('stocksTableResult =\n', stocksTableResult);
+    const createStocksTableQuery = {
+      text: `
+        CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+        CREATE TABLE IF NOT EXISTS stocks(
+          product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          count INT,
+          CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products (id)
+        )`
+    };
+    const stocksTableResult = await client.query(createStocksTableQuery);
+    console.log('stocksTableResult =\n', stocksTableResult);
+
+    async function fillDB () {
+      for (let i = 0; i < mockData.length; i++) {
+        const item = mockData[i];
+        const fillProductsQuery = {
+          text: 'INSERT INTO products (title, description, price) VALUES ($1, $2, $3) RETURNING id',
+          values: [item.title, item.description, item.price]
+        };
+        const fillProductsResult = await client.query(fillProductsQuery);
+        console.log('fillProductsResult =\n', fillProductsResult);
+
+        const id = fillProductsResult.rows[0].id;
+        const fillStocksQuery = {
+          text: 'INSERT INTO stocks (product_id, count) VALUES ($1, $2)',
+          values: [id, item.count]
+        };
+        const fillStocksResult = await client.query(fillStocksQuery);
+        console.log('fillStocksResult =\n', fillStocksResult);
+      }
+    }
+
+    fillDB();
+
   } catch (error) {
     console.log('initDB error = ', error);
   } finally {
