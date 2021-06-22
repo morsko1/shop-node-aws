b86618420107f68d054bdb6e869cc198b4a4f0e7
morsko1 <morssko1@gmail.com>
2021-06-22 12:44:00 +0300

    updated tests after integration with db

--- product-service/__tests__/getProductsById.test.js
+++ product-service/__tests__/getProductsById.test.js
@@ -1,5 +1,15 @@
 import {getProductsById} from '../handler';
 import mockData from '../mockData.json';
+import { Client } from 'pg';
+
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Client: jest.fn(() => mClient) };
+});
 
 const successResponse = {
   statusCode: 200,
@@ -31,11 +41,21 @@ const failureResponse = {
 
 describe('getProductsById', () => {
 
+  let client;
+  beforeEach(() => {
+    client = new Client();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('getProductsById: success', () => {
+    client.query.mockResolvedValue({ rows: [mockData[4]], rowCount: 1 });
     expect(getProductsById({"pathParameters":{"productId":"4"}})).resolves.toEqual(successResponse);
   });
 
   test('getProductsById: not found', () => {
+    client.query.mockResolvedValue({ rows: [], rowCount: 0 });
     expect(getProductsById({"pathParameters":{"productId":"44"}})).resolves.toEqual(notFoundResponse);
   });
 
--- product-service/__tests__/getProductsList.test.js
+++ product-service/__tests__/getProductsList.test.js
@@ -1,5 +1,15 @@
 import {getProductsList} from '../handler';
 import mockData from '../mockData.json';
+import { Client } from 'pg';
+
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Client: jest.fn(() => mClient) };
+});
 
 const successResponse = {
   statusCode: 200,
@@ -13,7 +23,16 @@ const successResponse = {
 
 describe('getProductsList', () => {
 
+  let client;
+  beforeEach(() => {
+    client = new Client();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('getProductsList: success', async () => {
+    client.query.mockResolvedValue({ rows: mockData, rowCount: 8 });
     expect(getProductsList()).resolves.toEqual(successResponse);
     const res = await getProductsList();
     const body = JSON.parse(res.body);
