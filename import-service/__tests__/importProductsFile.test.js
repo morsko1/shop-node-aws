f6a661f3f24f06dd9db48c32171603e4f24c5ec2
morsko1 <morssko1@gmail.com>
2021-06-25 13:26:14 +0300

    added tests for importProductsFile function

---
+++ import-service/__tests__/importProductsFile.test.js
@@ -0,0 +1,36 @@
+import { importProductsFile } from '../handler';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+jest.mock('@aws-sdk/s3-request-presigner', () => {
+  return { getSignedUrl: jest.fn() };
+});
+
+const successResponse = {
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+  body: JSON.stringify('https://mysignedurl.com/test.csv')
+};
+
+const failureResponse = {
+  statusCode: 500,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  },
+  body: JSON.stringify({message: 'Internal server error'})
+};
+
+fdescribe('importProductsFile', () => {
+
+  test('importProductsFile: success', () => {
+    getSignedUrl.mockResolvedValue('https://mysignedurl.com/test.csv');
+    expect(importProductsFile({"queryStringParameters":{"name":"test.csv"}})).resolves.toEqual(successResponse);
+  });
+
+  test('importProductsFile: failure', () => {
+    getSignedUrl.mockResolvedValue('https://mysignedurl.com/test.csv');
+    expect(importProductsFile({"pathParameters":{"name":"test.csv"}})).resolves.toEqual(failureResponse);
+  });
+
+});
