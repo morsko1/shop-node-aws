398354076e25c92528b09d776a16519d1fdbb99f
morsko1 <morssko1@gmail.com>
2021-09-17 14:48:53 +0300

    added requests handling in bff-service

--- bff-service/index.js
+++ bff-service/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
@@ -7,9 +8,43 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3001;
 
-app.use('*', (req, res) => {
-  console.log('req = ', req);
-  res.send({success: true});
+app.use('*', async (req, res) => {
+  try {
+    // prepare URL to route incoming request
+    const originalUrl = req.originalUrl;
+    console.log('originalUrl = ', originalUrl);
+    const recipientServiceName = originalUrl.split('/')[1];
+    const serviceUrl = process.env[recipientServiceName];
+    console.log('serviceUrl = ', serviceUrl);
+    const recipientPath = originalUrl.slice(originalUrl.indexOf('/', 1));
+    console.log('recipientPath = ', recipientPath);
+    const recipientURL = serviceUrl + recipientPath;
+    console.log('recipientURL = ', recipientURL);
+
+    // prepare the request (body if necessary)
+    const method = req.method;
+    const body = req.body;
+    console.log('method = ', method);
+    console.log('body = ', body);
+
+    const requestConfig = {
+      method,
+      url: recipientURL,
+      ...( (body && Object.keys(body).length > 0) && {data: body} )
+    };
+    console.log('requestConfig = ', requestConfig);
+
+    // send the request to appropriate server
+
+    const response = await axios(requestConfig);
+    console.log('typeof response = ', typeof response.data);
+    console.log('response = ', response.data);
+
+    res.send(JSON.stringify(response.data));
+  } catch(error) {
+    console.log('error = ', error);
+    res.status(502).send('Couldn\'t process the request');
+  }
 });
 
 app.listen(port, () => {
